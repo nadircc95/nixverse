964980482f7eeed7b9643b0b78844989c72d29ff
nadir <nadir.saja09@gmail.com>
2026-01-19 17:03:33 +0000

    initial wsl nixos

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Nix Config Personal";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-wsl.url = "github:nix-community/nixos-wsl/main";
+
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl }: {
+    nixosConfigurations = {
+      nadir-wsl = nixpkgs.lib.nixosSystem {
+	system = "x86_64-linux";
+	modules = [
+	  nixos-wsl.nixosModules.default
+	  {
+	    system.stateVersion = "25.11";
+	    wsl.enable = true;
+	    wsl.defaultUser = "nadir";
+
+	    nix.settings.experimental-features = [
+	      "nix-command"
+	      "flakes"
+	    ];
+	  }
+	];
+      };
+    };
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
