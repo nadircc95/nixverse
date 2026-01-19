2cd5e7a95a236b944643c6d2bd83f28d5169e7f9
nadir <nadir.saja09@gmail.com>
2026-01-19 17:38:49 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# nix configuration
+
+## command 
+
+* Nix Verse Rebuild 
+```console
+sudo nixos-rebuild switch --flake github:nadircc95/nixverse#nadir-wslm
+```
--- flake.nix
+++ flake.nix
@@ -27,9 +27,5 @@
       };
     };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
