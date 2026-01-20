641f01dca018ff2c3004c61b57ba356a94f28bf4
nadir <nadir.saja09@gmail.com>
2026-01-20 08:47:44 +0000

    add telescope

--- neovim.nix
+++ neovim.nix
@@ -28,7 +28,38 @@
 	action = "<c-w>k";
 	key = "<c-k>";
       }	
+      {
+	action = "<cmd>Telescope<cr>";
+	key = "<c-f>";
+      }
       ];
+      # config.lazyload.settings.cmd = "Telescope";
+      config.plugins.telescope = {
+	enable = true;
+	keymaps.ff.options.desc = "Find by files";
+	keymaps.ff.action = "find_files";
+
+	keymaps.fF.options.desc = "Find by words";
+	keymaps.fF.action = "live_grep";
+
+	keymaps."f'".options.desc = "Find by string";
+	keymaps."f'".action = "grep_string";
+
+	keymaps.fb.options.desc = "Find by current buffers";
+	keymaps.fb.action = "buffers";
+
+	keymaps.fB.options.desc = "Find fuzz by current buffers";
+	keymaps.fB.action = "current_buffer_fuzzy_find";
+
+	keymaps.fh.options.desc = "Find by help tags";
+	keymaps.fh.action = "help_tags";
+
+	keymaps.fc.options.desc = "Find by colorscheme";
+	keymaps.fc.action = "colorscheme";
+
+	keymaps.fC.options.desc = "Find by highlights";
+	keymaps.fC.action = "highlights";
+      };
       config.colorschemes.vscode.enable = true;
       config.colorschemes.vscode.settings = {
 	color_overrides = {
@@ -44,7 +75,7 @@
 	enable = true;
 	settings = {
 	  view.side = "left";
-	  view.width = 25;
+	  view.width = 45;
 	  filters.dotfiles = true;
 	  git.enable = true;
 	};
