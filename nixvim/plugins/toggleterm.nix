ad29e300a657cc6e486d293691352b119606dbf3
nadir <nadir.saja09@gmail.com>
2026-04-21 10:42:36 +0000

    Fix toggleterm plugin syntax

--- nixvim/plugins/toggleterm.nix
+++ nixvim/plugins/toggleterm.nix
@@ -2,17 +2,15 @@
 {
   plugins.toggleterm = {
     enable = true;
-    settings = {
-      direction = "float";
-      float_opts = {
-        border = "curved";
-        winblend = 3;
-      };
-      open_mapping = "<C-\\>";
-      insert_mappings = true;
-      terminal_mappings = true;
-      persist_size = true;
-      close_on_exit = true;
+    direction = "float";
+    openMapping = "<C-\\>";
+    insertMappings = true;
+    terminalMappings = true;
+    persistSize = true;
+    closeOnExit = true;
+    floatOpts = {
+      border = "curved";
+      winblend = 3;
     };
   };
 }
