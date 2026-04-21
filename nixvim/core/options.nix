b30e9aa96769d566db2459c8e3b58d527f12b7a7
nadir <nadir.saja09@gmail.com>
2026-04-21 06:28:37 +0000

    Add Blade syntax support with filetype detection and Treesitter grammar

--- nixvim/core/options.nix
+++ nixvim/core/options.nix
@@ -20,4 +20,31 @@
     updatetime = 300;
     timeoutlen = 400;
   };
+
+  config.extraConfigLua = ''
+    -- 1. Deteksi filetype
+    vim.filetype.add({
+      pattern = { ['.*%.blade%.php'] = 'blade' },
+    })
+
+    -- 2. Daftarkan parser PHP untuk Blade (Kunci Utama Warna)
+    -- Kita beritahu Neovim: "Kalau ada file Blade, pakai logika PHP/HTML"
+    vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+      pattern = "*.blade.php",
+      callback = function()
+        vim.treesitter.start(0, "php") 
+        vim.cmd("setlocal syntax=php")
+      end,
+    })
+
+    -- 3. Tambahkan Query Injection langsung di memori
+    local query = [[
+      ((text) @injection.content
+       (#set! injection.combined)
+       (#set! injection.language "html"))
+    ]]
+
+    -- Pastikan parser php sudah dimuat dulu sebelum set query
+    pcall(require("vim.treesitter.query").set, "php", "injections", query)
+  '';
 }
--- nixvim/plugins/treesitter.nix
+++ nixvim/plugins/treesitter.nix
@@ -4,10 +4,31 @@
     enable = true;
 
     nixGrammars = true;
+    nixvimInjections = true;
+
+    grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
+      lua
+      vim
+      vimdoc
+      bash
+      json
+      yaml
+      toml
+      php
+      php_only
+      phpdoc
+      blade
+      html
+      css
+      javascript
+      typescript
+      sql
+      markdown
+    ];
 
     settings = {
-      highlight.enable = true;
       indent.enable = true;
+      highlight.enable = true;
     };
   };
 }
